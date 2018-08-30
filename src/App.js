1c4fadc9144c205a399481755495fb39cd241cb2
dkwazhere <dhealth77@gmail.com>
2018-08-29 19:18:39 -0700

    added reactmaterialize

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Button, Icon} from 'react-materialize'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Juul Korea</h1>
+        <p>쥴코리아.</p>
+        <Button waves='light'>
+          <Icon>thumb_up</Icon>
+        </Button>
       </div>
     );
   }

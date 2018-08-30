940f9c2ad30fea759ef7a3fc6bc4bef7c3d0c15d
dkwazhere <dhealth77@gmail.com>
2018-08-30 02:33:23 -0700

    added basic navbar with scrollspy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Button, Icon} from 'react-materialize'
+import { Route, NavLink, HashRouter} from 'react-router-dom';
+import Home from './pages/Home';
+import {  Col, Container } from "reactstrap";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Juul Korea</h1>
-        <p>쥴코리아.</p>
-        <Button waves='light'>
-          <Icon>thumb_up</Icon>
-        </Button>
+      <HashRouter>
+          <Container className="main">
+              <ul className="header">
+              <h1 id="main-name">Juul Korea</h1>
+                <a><NavLink exact to="/">Home / </NavLink></a>
+                <a><NavLink to="/about">About Us / </NavLink></a>
+                <a><NavLink to="/shop">Shop / </NavLink></a>
+                <a><NavLink to="/contact">Contact</NavLink></a>
+              </ul>
+
+              <div className="content">
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" component={Home}/>
+                <Route path="/Shop" component={Home}/>
+                <Route path="/contact" component={Home}/>
+              </div>
+          </Container>
+        </HashRouter>
       </div>
     );
   }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+import Scrollspy from 'react-scrollspy';
+
+
+class Home extends Component {
+    render() {
+        return(
+            <div className="home">
+            
+            </div>
+        );
+    }
+}
+
+export default Home;

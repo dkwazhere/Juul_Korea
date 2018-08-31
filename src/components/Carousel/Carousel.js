030ab090691777662594c3c1c18ac017791fcf52
dkwazhere <dhealth77@gmail.com>
2018-08-31 08:06:11 -0700

    added juul flavors into carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,39 +1,56 @@
 import React from "react";
 import Slider from "react-slick";
 import './Carousel.css';
+import Classicmenthol from '../../assets/images/flavors/classicmenthol.jpg';
+import Classictobacco from '../../assets/images/flavors/classictobacco.jpg';
+import Coolcucumber from '../../assets/images/flavors/coolcucumber.jpg';
+import Coolmint from '../../assets/images/flavors/coolmint.jpg';
+import Cremebrulee from '../../assets/images/flavors/cremebrulee.jpg';
+import Fruitmedley from '../../assets/images/flavors/fruitmedley.jpg';
+import Mango from '../../assets/images/flavors/mango.jpg';
+import Virginiatobacco from '../../assets/images/flavors/virginiatobacco.jpg';
 
 class Carousel extends React.Component {
-  render() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-    return (
-      <Slider {...settings}>
+    render() {
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      };
+      return (
         <div>
-          <img src='https://avatars0.githubusercontent.com/u/29829664?s=460&v=4'></img>
+          <h2> Multiple items </h2>
+          <Slider {...settings}>
+            <div>
+              <img src={Classicmenthol}></img>
+            </div>
+            <div>
+              <img src={Classictobacco}></img>
+            </div>
+            <div>
+              <img src={Coolcucumber}></img>
+            </div>
+            <div>
+              <img src={Coolmint}></img>
+            </div>
+            <div>
+              <img src={Cremebrulee}></img>
+            </div>
+            <div>
+              <img src={Fruitmedley}></img>
+            </div>
+            <div>
+              <img src={Mango}></img>
+            </div>
+            <div>
+              <img src={Virginiatobacco}></img>
+            </div>
+          </Slider>
         </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-    );
+      );
+    }
   }
-}
 
 export default Carousel;

4018e0abe53859ec4bf01245f7aaa75b4bd26e11
dkwazhere <dhealth77@gmail.com>
2018-09-02 07:01:37 -0700

    added juultech-side images and headers

--- src/components/Juultech/Juultech.js
+++ src/components/Juultech/Juultech.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './Juultech.css';
 import Juuldeviceimg from '../../assets/images/juul-device.png';
+import Juultechcheck from '../../assets/images/juultechnology/juultech-check.png';
+import Juultechcloud from '../../assets/images/juultechnology/juultech-cloud.png';
+import Juultechsettings from '../../assets/images/juultechnology/juultech-setting.png';
 
 const Juultech = (props) => {
   return (
     <div className='juultech-container'>
         <img className='juuldevpic' alt='juuldevpic' src={Juuldeviceimg}></img>
-        <div className='juultech-side'>Juul img</div>
+        <div className='juultech-side'>
+            <div>
+                <div><img src={Juultechcheck}></img></div>
+                <div>
+                    <h6>Simplicity</h6>
+                </div>
+            </div>
+
+            <div>
+                <div><img src={Juultechcloud}></img></div>
+                <div>
+                    <h6>Clean</h6>
+                </div>
+            </div>
+
+            <div>
+                <div><img src={Juultechsettings}></img></div>
+                <div>
+                <h6>Satisfying</h6>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };

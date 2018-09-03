70b831dc66fa4bc1904d7b11e23f3c9f145fbfb8
dkwazhere <dhealth77@gmail.com>
2018-09-03 06:11:18 -0700

    added juulinfo component, component will be going under about page

--- src/components/Aboutjumbotron/Aboutjumbotron.js
+++ src/components/Aboutjumbotron/Aboutjumbotron.js
@@ -7,9 +7,10 @@ const Aboutjumbotron = (props) => {
   return (
     <div className='Jumbotron'>
       <Jumbotron>
-        <h1 className="display-3">About the Juul</h1>
+        <h1 className="display-3">Juul Technology</h1>
         <img src={Aboutjumbopic} alt='aboutjumbopic'></img>
       </Jumbotron>
+      <div className='about-intro'>JUUL Labs was founded with the goal of improving the lives of the world’s one billion adult smokers by providing them with an easy to use vapor alternative to combustible cigarettes, containing a similar level of nicotine to make switching as easy as possible.</div>
     </div>
   );
 };
---
+++ src/components/Juulinfo/Juulinfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Jumbotron, Button } from 'reactstrap';
+import './Jumbotron.css';
+
+const Example = (props) => {
+  return (
+    <div className='juul-info'>
+        
+    </div>
+  );
+};
+
+export default Example;

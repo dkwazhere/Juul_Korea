91672e50c1b1751028c09f38eaa327e87661e234
dkwazhere <dhealth77@gmail.com>
2018-09-07 21:35:59 -0700

    removed unecessary forgroup

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,7 +6,6 @@ class Contact extends Component {
     render() {
         return(
             <div className='contact'>
-                <h1>Contact Us</h1>
                 <Contactform />
             </div>
         )

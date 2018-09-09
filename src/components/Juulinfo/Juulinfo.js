d0313cdc95273ccd1496572dcf309d63c4fcc256
dkwazhere <dhealth77@gmail.com>
2018-09-09 03:37:10 -0700

    added font from google fonts

--- src/components/Juulinfo/Juulinfo.js
+++ src/components/Juulinfo/Juulinfo.js
@@ -7,7 +7,7 @@ const Juulinfo = (props) => {
   return (
     <div className='juul-info'>
         <div className='juul-device-info'>
-            <h5>What is a Juul device?</h5>
+            <h4>What is a Juul device?</h4>
             <a>JUUL uses an heating mechanism that creates <br/> an aerosol and is engineered to minimize combustion. <br/><br/></a>
             <a>JUUL is a closed system vapor product <br/> and is not designed to be refillable.<br/><br/></a>
             <a>JUUL is rechargeable via a USB port.</a>
@@ -17,12 +17,12 @@ const Juulinfo = (props) => {
         </div>
         <div className='juul-pod-info'>
         <div className='horizontal-rule'><hr/></div>
-            <h5>What is a Juul Pod?</h5>
+            <h4>What is a Juul Pod?</h4>
             <a>A JUULpod is the cartridge that clicks into the top <br/> of the JUUL device. </a>
             <a>JUULpods contain a proprietary<br/> salt-based nicotine e-liquid formula, which is mixed <br/>under strict quality-controlled processes, and is intended <br/>to help satisfy smokers when transitioning from cigarettes.<br/><br/></a>
-            <h5>What is an E-liquid?</h5>
+            <h4>What is an E-liquid?</h4>
             <a>An e-liquid is the fluid used in vapor products to create the actual vapor.<br/><br/></a>
-            <h5>What is Vapor?</h5>
+            <h4>What is Vapor?</h4>
             <a>Vapor is the aerosol that is generated when an e-liquid is heated.</a>
             <br/>
             <br/>
